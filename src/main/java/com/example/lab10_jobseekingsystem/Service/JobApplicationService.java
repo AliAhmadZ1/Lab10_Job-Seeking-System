79389981ac260f891e9f9be6bd16d7f1c7419683
aliali <aliali@test.com>
2025-04-07 13:45:46 +0300

    update apply and withdraw method

--- src/main/java/com/example/lab10_jobseekingsystem/Service/JobApplicationService.java
+++ src/main/java/com/example/lab10_jobseekingsystem/Service/JobApplicationService.java
@@ -1,7 +1,11 @@
 package com.example.lab10_jobseekingsystem.Service;
 
 import com.example.lab10_jobseekingsystem.Model.JobApplication;
+import com.example.lab10_jobseekingsystem.Model.JobPost;
+import com.example.lab10_jobseekingsystem.Model.User;
 import com.example.lab10_jobseekingsystem.Repository.JobApplicationRepository;
+import com.example.lab10_jobseekingsystem.Repository.JobPostRepository;
+import com.example.lab10_jobseekingsystem.Repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +16,39 @@ import java.util.List;
 public class JobApplicationService {
 
     private final JobApplicationRepository jobApplicationRepository;
+    private final UserRepository userRepository;
+    private final JobPostRepository jobPostRepository;
 
-    public List<JobApplication> getAllJobApplication(){
+    public List<JobApplication> getAllJobApplication() {
         return jobApplicationRepository.findAll();
     }
 
-    public void applyForJob(JobApplication jobApplication){
-        jobApplicationRepository.save(jobApplication);
+    public Boolean applyForJob(JobApplication jobApplication) {
+        for (User u : userRepository.findAll()) {
+            if (u.getId() == jobApplication.getUser_id()) {
+                for (JobPost j : jobPostRepository.findAll()) {
+                    if (j.getId() == jobApplication.getJob_post_id()) {
+                        for (JobApplication ja : jobApplicationRepository.findAll()) {
+                            if (ja.getUser_id() == u.getId() && ja.getJob_post_id() == j.getId())
+                                return false;
+                        }
+                        jobApplicationRepository.save(jobApplication);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
-    public Boolean withdrawJobApplication(Integer id){
-        JobApplication oldJobApplication = jobApplicationRepository.getById(id);
-        if (oldJobApplication==null)
-            return false;
-
-        jobApplicationRepository.delete(oldJobApplication);
-        return true;
+    public Boolean withdrawJobApplication(Integer id) {
+        for (JobApplication ja : jobApplicationRepository.findAll()) {
+            if (ja.getId() == id) {
+//                JobApplication oldJobApplication = jobApplicationRepository.getById(id);
+                jobApplicationRepository.delete(ja);
+                return true;
+            }
+        }
+        return false;
     }
 }
