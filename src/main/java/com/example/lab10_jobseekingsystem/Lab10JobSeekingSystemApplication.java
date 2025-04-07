1db48dcd6d4665d794732d897035d0b998d7128b
aliali <aliali@test.com>
2025-04-07 10:48:05 +0300

    .

--- src/main/java/com/example/lab10_jobseekingsystem/Lab10JobSeekingSystemApplication.java
+++ src/main/java/com/example/lab10_jobseekingsystem/Lab10JobSeekingSystemApplication.java
@@ -10,4 +10,16 @@ public class Lab10JobSeekingSystemApplication {
         SpringApplication.run(Lab10JobSeekingSystemApplication.class, args);
     }
 
+    //1. Get all Users: Retrieves a list of all Users.
+    //2. Add a new User: Adds a new user to the system.
+    //3. Update a User: Updates an existing user’s information.
+    //4. Delete a User: Deletes a user from the system. Note: Verify that the user exists.
+    //5. Get all JobPosts: Retrieves a list of all Job Posts.
+    //6. Add a new JobPost: Adds a new job post to the system.
+    //7. Update a JobPost: Updates an existing job post ’s information.
+    //8. Delete a JobPost: Deletes a job post from the system. Note: Verify that the job post exists.
+    //9. Get all Job Applications: Retrieves a list of all Job Application.
+    //10. Apply For Job: Adds a new job application to the system.
+    //11. Withdraw Job Application: Deletes a job application from the system.
+    //Note: Verify that the job application exists.
 }
