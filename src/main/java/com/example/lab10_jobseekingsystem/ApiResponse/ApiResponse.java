f01a2ca735a444574e72e7320dfa3c2a3f0de06b
aliali <aliali@test.com>
2025-04-07 10:25:42 +0300

    update

--- src/main/java/com/example/lab10_jobseekingsystem/ApiResponse/ApiResponse.java
+++ src/main/java/com/example/lab10_jobseekingsystem/ApiResponse/ApiResponse.java
@@ -1,4 +1,10 @@
 package com.example.lab10_jobseekingsystem.ApiResponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiResponse {
+    private String message;
 }
